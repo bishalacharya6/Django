964644234e747484aa8c951fd13bb7f219eb1e62
bishalacharya6 <bishalacharya673@gmail.com>
2024-05-06 11:39:21 +0545

    asd

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,25 @@
+// it will intercept and use automatic headers
+// Settig axios request tokens. 
+
 import { ACCESS_TOKEN } from "./constants"
 import axios from axios
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
+    baseURL: import.meta.env.VITE_API_URL // Import anything that is specify in any env variabls. 
 })
+
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem(ACCESS_TOKEN);
+        if (token){
+            config.headers.Authorization = `Bearer ${token}` //The way for using jwt token
+        }
+        return config
+    },
+    (error)=>{
+        return Promise.reject(error)
+    }
+    
+)
+
+export default api
